546b6b1ba3ab915b2cab09b23808211a57be5b77
serendi <bmhatab@gmail.com>
2023-04-15 21:22:04 +0800

    update

--- main/config.py
+++ main/config.py
@@ -3,15 +3,15 @@
 
 class Config:
     # init_session
-    phone = "+639630663848"
+    phone = "xxx9630663848"
     api_id = "9228388"
-    api_hash = "b1a2e80b150dc55a5e67718647eeb785"
+    api_hash = "b1a2e80xxx"
 
     # json_to_excel(Integer)
     source_group_id = 1588361928
     
     # add_to_group (Link)
-    main_group = "https://t.me/cryptoplanetit"
+    main_group = "xxx"
     count = 270
     end_count = 400
     accounts = 3
